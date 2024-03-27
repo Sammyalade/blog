ff7ccd37a39b36c0542b0ee6843d54cc4f810423
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-27 22:04:54 +0100

    blog

--- src/main/java/africa/semicolon/blog/datas/models/View.java
+++ src/main/java/africa/semicolon/blog/datas/models/View.java
@@ -1,11 +1,18 @@
 package africa.semicolon.blog.datas.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Data
 @Document("Views")
 public class View {
 
+    @Id
+    private String id;
+    private User viewer;
+    private LocalDateTime viewTime;
 
 }
