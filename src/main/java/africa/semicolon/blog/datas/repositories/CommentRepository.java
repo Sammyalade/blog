fcbf8d33a7a5d87402c95ac0144396945a6870f0
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-27 22:05:06 +0100

    blog

---
+++ src/main/java/africa/semicolon/blog/datas/repositories/CommentRepository.java
@@ -0,0 +1,7 @@
+package africa.semicolon.blog.datas.repositories;
+
+import africa.semicolon.blog.datas.models.Comment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CommentRepository extends MongoRepository<Comment, String> {
+}
