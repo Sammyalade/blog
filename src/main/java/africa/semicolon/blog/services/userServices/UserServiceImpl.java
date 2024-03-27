99b6a53cdbfd4f9fe718240858e0b0b0ea901b8e
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-27 22:37:10 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/userServices/UserServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/userServices/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package africa.semicolon.blog.services.userServices;
 
 import africa.semicolon.blog.services.userServices.UserService;
+import org.springframework.stereotype.Service;
 
+@Service("UserServiceImpl")
 public class UserServiceImpl implements UserService {
 }
