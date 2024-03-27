9b462968df162aa94811e915d87e0df618dbc32a
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-27 08:36:06 +0100

    blog

---
+++ src/main/java/africa/semicolon/blog/datas/models/User.java
@@ -0,0 +1,22 @@
+package africa.semicolon.blog.datas.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Document("Users")
+public class User {
+
+    @Id
+    private String id;
+    private String  firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    @DBRef
+    private List<Post> posts;
+}
