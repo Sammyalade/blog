7ade40b7dae3e6579c68ecfcec82e84ad0b175a9
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-27 22:38:06 +0100

    blog

--- src/test/java/africa/semicolon/blog/services/UserServiceTest.java
+++ src/test/java/africa/semicolon/blog/services/UserServiceTest.java
@@ -1,4 +1,8 @@
 package africa.semicolon.blog.services;
 
+import africa.semicolon.blog.Main;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Main.class)
 public class UserServiceTest {
 }
