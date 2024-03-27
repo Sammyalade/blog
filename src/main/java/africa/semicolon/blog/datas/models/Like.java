92cef29ebc2091f54630f9502c317d496f7c0bb8
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-27 22:04:40 +0100

    blog

--- src/main/java/africa/semicolon/blog/datas/models/Like.java
+++ src/main/java/africa/semicolon/blog/datas/models/Like.java
@@ -1,9 +1,13 @@
 package africa.semicolon.blog.datas.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document("Likes")
 public class Like {
+    @Id
+    private String id;
+    private User likedBy;
 }
