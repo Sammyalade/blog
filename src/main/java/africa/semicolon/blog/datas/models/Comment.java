3853643390cb6e5616cd85885b1899ff53a9bb03
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-27 08:35:12 +0100

    blog

---
+++ src/main/java/africa/semicolon/blog/datas/models/Comment.java
@@ -0,0 +1,17 @@
+package africa.semicolon.blog.datas.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@Document("Comments")
+public class Comment {
+    @Id
+    private String id;
+    private LocalDate timeOfView;
+    private User viewer;
+}
