6c5591e06af0d45baf935f2d78aaf82fd70cf734
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-27 22:36:28 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/likeServices/LikeServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/likeServices/LikeServiceImpl.java
@@ -1,6 +1,8 @@
 package africa.semicolon.blog.services.likeServices;
 
 import africa.semicolon.blog.services.likeServices.LikeService;
+import org.springframework.stereotype.Service;
 
+@Service("likeServiceImpl")
 public class LikeServiceImpl implements LikeService {
 }
