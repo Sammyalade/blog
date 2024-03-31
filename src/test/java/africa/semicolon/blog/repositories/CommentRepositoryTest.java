0929b53c047a05fc6938d87a3469b124eb8f7b91
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 00:39:53 +0100

    blog

--- src/test/java/africa/semicolon/blog/repositories/CommentRepositoryTest.java
+++ src/test/java/africa/semicolon/blog/repositories/CommentRepositoryTest.java
@@ -6,12 +6,13 @@ import africa.semicolon.blog.datas.repositories.CommentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@SpringBootTest(classes = Main.class)
+@DataMongoTest
 public class CommentRepositoryTest {
 
     @Autowired
