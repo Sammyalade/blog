b0424e4129c1150c458372fd31f59fdcbd3fb736
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 00:40:31 +0100

    blog

--- src/test/java/africa/semicolon/blog/services/PostServiceTest.java
+++ src/test/java/africa/semicolon/blog/services/PostServiceTest.java
@@ -1,8 +1,67 @@
 package africa.semicolon.blog.services;
 
-import africa.semicolon.blog.Main;
+import africa.semicolon.blog.datas.repositories.PostRepository;
+import africa.semicolon.blog.dtos.request.postRequest.PostCreationRequest;
+import africa.semicolon.blog.dtos.request.postRequest.PostDeleteRequest;
+import africa.semicolon.blog.exceptions.EmptyStringException;
+import africa.semicolon.blog.services.postServices.PostService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = Main.class)
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
 public class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @BeforeEach
+    public void setUp() {
+        postRepository.deleteAll();
+    }
+    @Test
+    public void makePost_checkPostRepository_numberOfPostIsOneTest(){
+        PostCreationRequest postCreationRequest = new PostCreationRequest();
+        postCreationRequest.setTitle("Title");
+        postCreationRequest.setContent("Content");
+        postService.makePost(postCreationRequest);
+        assertThat(postRepository.count(), is(1L));
+    }
+
+    @Test
+    public void makePostWithEmptyStringTitle_throwsEmptyStringExceptionTest(){
+        PostCreationRequest postCreationRequest = new PostCreationRequest();
+        postCreationRequest.setTitle("");
+        postCreationRequest.setContent("Content");
+        assertThrows(EmptyStringException.class, ()-> postService.makePost(postCreationRequest));
+    }
+
+    @Test
+    public void makePostWithEmptyStringContent_throwsEmptyStringExceptionTest(){
+        PostCreationRequest postCreationRequest = new PostCreationRequest();
+        postCreationRequest.setTitle("Title");
+        postCreationRequest.setContent("");
+        assertThrows(EmptyStringException.class, ()-> postService.makePost(postCreationRequest));
+    }
+
+    @Test
+    public void makePost_deletePost_postIsDeletedTest(){
+        PostCreationRequest postCreationRequest = new PostCreationRequest();
+        postCreationRequest.setTitle("Title");
+        postCreationRequest.setContent("Content");
+        postService.makePost(postCreationRequest);
+        PostDeleteRequest postDeleteRequest = new PostDeleteRequest();
+        postDeleteRequest
+        postService.deletePost(postDeleteRequest);
+        assertThat(postRepository.count(), is(1L));
+    }
+
 }
