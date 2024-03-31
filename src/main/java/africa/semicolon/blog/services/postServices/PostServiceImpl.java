00b258de72008f3129c994d26e74f97c40b95ec3
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 00:58:05 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/postServices/PostServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/postServices/PostServiceImpl.java
@@ -4,6 +4,7 @@ import africa.semicolon.blog.datas.models.Post;
 import africa.semicolon.blog.datas.repositories.PostRepository;
 import africa.semicolon.blog.dtos.request.postRequest.*;
 import africa.semicolon.blog.exceptions.EmptyStringException;
+import africa.semicolon.blog.exceptions.UniqueTitleException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,21 @@ public class PostServiceImpl implements PostService {
     public void makePost(PostCreationRequest postCreationRequest) {
         Post post = map(postCreationRequest);
         validateForEmptyString(post);
+        checkForUniqueTitle(postCreationRequest);
         postRepository.save(post);
+    }
+
+    private void checkForUniqueTitle(PostCreationRequest postCreationRequest) {
+        if (findPost(postCreationRequest.getTitle()) != null) throw new UniqueTitleException("Title has been used. Tiyle has to be unique");
+    }
+
+    private Post findPost(String title) {
+        for(Post post : postRepository.findAll()) {
+            if(post.getTitle().equals(title)) {
+                return post;
+            }
+        }
+        return null;
     }
 
     private static void validateForEmptyString(Post post) {
@@ -28,7 +43,9 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public void deletePost(String title) {
-
+        for(Post post : postRepository.findAll()) {
+            if(post.getTitle().equals(title)) postRepository.delete(post);
+        }
     }
 
     @Override
