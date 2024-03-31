9f370b0abf25baa510ee62d7e52b5d6caba77ba5
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 00:36:18 +0100

    blog

--- src/main/java/africa/semicolon/blog/datas/models/Post.java
+++ src/main/java/africa/semicolon/blog/datas/models/Post.java
@@ -6,17 +6,18 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@Document("Posts")
+@Document
 public class Post {
 
     @Id
     private String id;
     private String title;
     private String content;
-    private LocalDate dateCreated;
+    private LocalDateTime dateCreated = LocalDateTime.now();
     @DBRef
     private List<View> views;
     @DBRef
