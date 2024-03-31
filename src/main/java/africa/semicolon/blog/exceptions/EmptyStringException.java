917303a17255801d51da924217d9b6e0c71838d0
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 00:38:47 +0100

    blog

---
+++ src/main/java/africa/semicolon/blog/exceptions/EmptyStringException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.blog.exceptions;
+
+public class EmptyStringException extends BlogException {
+    public EmptyStringException(String message) {
+        super(message);
+    }
+}
