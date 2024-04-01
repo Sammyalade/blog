14b6185435125f604f4c0185e9a8e7353d8ba3ba
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:06:07 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/viewServices/ViewService.java
+++ src/main/java/africa/semicolon/blog/services/viewServices/ViewService.java
@@ -1,4 +1,7 @@
 package africa.semicolon.blog.services.viewServices;
 
+import africa.semicolon.blog.dtos.request.postRequest.PostViewRequest;
+
 public interface ViewService {
+    void viewPost(PostViewRequest postViewRequest);
 }
