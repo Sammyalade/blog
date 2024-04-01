de94e09a7ca31775cddb6c700b03aa3c361934f9
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 21:45:38 +0100

    blog

--- src/test/java/africa/semicolon/blog/services/PostServiceTest.java
+++ src/test/java/africa/semicolon/blog/services/PostServiceTest.java
@@ -1,7 +1,10 @@
 package africa.semicolon.blog.services;
 
 import africa.semicolon.blog.datas.models.Post;
+import africa.semicolon.blog.datas.models.User;
+import africa.semicolon.blog.datas.repositories.CommentRepository;
 import africa.semicolon.blog.datas.repositories.PostRepository;
+import africa.semicolon.blog.dtos.request.postRequest.PostCommentRequest;
 import africa.semicolon.blog.dtos.request.postRequest.PostCreationRequest;
 import africa.semicolon.blog.dtos.request.postRequest.PostUpdateRequest;
 import africa.semicolon.blog.exceptions.EmptyStringException;
@@ -12,7 +15,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static africa.semicolon.blog.utils.PostUtility.findPost;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -27,9 +29,13 @@ public class PostServiceTest {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private CommentRepository commentRepository;
+
     @BeforeEach
     public void setUp() {
         postRepository.deleteAll();
+        commentRepository.deleteAll();
     }
     @Test
     public void makePost_checkPostRepository_numberOfPostIsOneTest(){
@@ -92,6 +98,28 @@ public class PostServiceTest {
         postService.updatePost(postUpdateRequest);
         Post post = findPost("Title", postRepository.findAll());
         assertThat(post.getContent(), is("New Content"));
+    }
+
+    @Test
+    public void tryToUpdateAPostThatIsNotAvailable_throwsPostNotFoundExceptionTest(){
+        PostUpdateRequest postUpdateRequest = new PostUpdateRequest();
+        postUpdateRequest.setTitle("Title");
+        postUpdateRequest.setContent("New Content");
+        assertThrows(PostNotFoundException.class, ()->postService.updatePost(postUpdateRequest));
+    }
+
+    @Test
+    public void makePost_addComment_commentIsAddedTest(){
+        PostCreationRequest postCreationRequest = new PostCreationRequest();
+        postCreationRequest.setTitle("Title");
+        postCreationRequest.setContent("Content");
+        postService.makePost(postCreationRequest);
+        User user = new User();
+        PostCommentRequest postCommentRequest = new PostCommentRequest();
+        postCommentRequest.setCommentBody("My comment");
+        postCommentRequest.setViewer(user);
+        postService.makeComment(postCommentRequest);
+        assertThat(commentRepository.count(), is(1L));
     }
 
 }
