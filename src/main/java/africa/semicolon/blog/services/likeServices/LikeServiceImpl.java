7b3dfb7faf42d31f9eaf739180122b992f469bd0
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:44:55 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/likeServices/LikeServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/likeServices/LikeServiceImpl.java
@@ -1,8 +1,13 @@
 package africa.semicolon.blog.services.likeServices;
 
+import africa.semicolon.blog.dtos.request.postRequest.PostLikeRequest;
 import africa.semicolon.blog.services.likeServices.LikeService;
 import org.springframework.stereotype.Service;
 
 @Service("likeServiceImpl")
 public class LikeServiceImpl implements LikeService {
+    @Override
+    public void makeLike(PostLikeRequest postLikeRequest) {
+
+    }
 }
