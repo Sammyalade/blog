6121697ca1e1895d0c204d509e75e335aa0d6a21
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 01:14:48 +0100

    blog

--- src/main/java/africa/semicolon/blog/utils/PostUtility.java
+++ src/main/java/africa/semicolon/blog/utils/PostUtility.java
@@ -1,4 +1,17 @@
 package africa.semicolon.blog.utils;
 
+import africa.semicolon.blog.datas.models.Post;
+
+import java.util.List;
+
 public class PostUtility {
+
+    public static Post findPost(String title, List<Post> posts) {
+        for(Post post : posts) {
+            if(post.getTitle().equals(title)) {
+                return post;
+            }
+        }
+        return null;
+    }
 }
