cada9dd42f25ae264d2f4697589471685e12fae6
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:05:21 +0100

    blog

--- src/main/java/africa/semicolon/blog/dtos/request/postRequest/PostViewRequest.java
+++ src/main/java/africa/semicolon/blog/dtos/request/postRequest/PostViewRequest.java
@@ -1,4 +1,10 @@
 package africa.semicolon.blog.dtos.request.postRequest;
 
+import africa.semicolon.blog.datas.models.User;
+import lombok.Data;
+
+@Data
 public class PostViewRequest {
+    private String postTitle;
+    private User viewer;
 }
