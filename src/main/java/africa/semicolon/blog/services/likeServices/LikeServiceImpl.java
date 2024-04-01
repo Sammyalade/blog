2e8a6464a0a5cec9c27b73d413710e5c93b295aa
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:49:43 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/likeServices/LikeServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/likeServices/LikeServiceImpl.java
@@ -1,5 +1,6 @@
 package africa.semicolon.blog.services.likeServices;
 
+import africa.semicolon.blog.datas.models.Like;
 import africa.semicolon.blog.dtos.request.postRequest.PostLikeRequest;
 import africa.semicolon.blog.services.likeServices.LikeService;
 import org.springframework.stereotype.Service;
