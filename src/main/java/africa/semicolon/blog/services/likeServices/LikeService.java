b98990904682303efb73c3f37c12dda5f05b3b22
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:44:41 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/likeServices/LikeService.java
+++ src/main/java/africa/semicolon/blog/services/likeServices/LikeService.java
@@ -1,4 +1,9 @@
 package africa.semicolon.blog.services.likeServices;
 
+import africa.semicolon.blog.dtos.request.postRequest.PostLikeRequest;
+
 public interface LikeService {
+
+
+    void makeLike(PostLikeRequest postLikeRequest);
 }
