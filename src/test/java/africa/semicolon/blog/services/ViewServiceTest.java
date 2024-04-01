f3de1f64d3689350303eb2cb4d8ebd17e2a6a52b
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:27:34 +0100

    blog

--- src/test/java/africa/semicolon/blog/services/ViewServiceTest.java
+++ src/test/java/africa/semicolon/blog/services/ViewServiceTest.java
@@ -1,8 +1,27 @@
 package africa.semicolon.blog.services;
 
-import africa.semicolon.blog.Main;
+import africa.semicolon.blog.datas.repositories.ViewRepository;
+import africa.semicolon.blog.services.viewServices.ViewService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = Main.class)
+@SpringBootTest
 public class ViewServiceTest {
+
+    @Autowired
+    private ViewService viewService;
+    @Autowired
+    private ViewRepository viewRepository;
+
+    @BeforeEach
+    public void setUp() {
+        viewRepository.deleteAll();
+    }
+
+    @Test
+    public void viewServiceTest() {
+
+    }
 }
