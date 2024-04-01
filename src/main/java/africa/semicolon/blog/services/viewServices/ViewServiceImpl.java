4f6b95207fbae6ec085db48faa1706e16c67fe72
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:06:19 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/viewServices/ViewServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/viewServices/ViewServiceImpl.java
@@ -1,7 +1,22 @@
 package africa.semicolon.blog.services.viewServices;
 
+import africa.semicolon.blog.datas.models.View;
+import africa.semicolon.blog.datas.repositories.ViewRepository;
+import africa.semicolon.blog.dtos.request.postRequest.PostViewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static africa.semicolon.blog.utils.Mapper.map;
+
 @Service("ViewServiceImpl")
-public class ViewServiceImpl {
+public class ViewServiceImpl implements ViewService{
+
+
+    @Autowired
+    private ViewRepository viewRepository;
+    @Override
+    public void viewPost(PostViewRequest postViewRequest) {
+        View view = map(postViewRequest);
+        viewRepository.save(view);
+    }
 }
