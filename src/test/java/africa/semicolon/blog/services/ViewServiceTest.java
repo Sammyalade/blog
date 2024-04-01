25090c302fec2d95507f10236a2a7c438b090c20
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:28:56 +0100

    blog

--- src/test/java/africa/semicolon/blog/services/ViewServiceTest.java
+++ src/test/java/africa/semicolon/blog/services/ViewServiceTest.java
@@ -1,11 +1,16 @@
 package africa.semicolon.blog.services;
 
+import africa.semicolon.blog.datas.models.User;
 import africa.semicolon.blog.datas.repositories.ViewRepository;
+import africa.semicolon.blog.dtos.request.postRequest.PostViewRequest;
 import africa.semicolon.blog.services.viewServices.ViewService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 public class ViewServiceTest {
@@ -22,6 +27,11 @@ public class ViewServiceTest {
 
     @Test
     public void viewServiceTest() {
-
+        User user = new User();
+        PostViewRequest postViewRequest = new PostViewRequest();
+        postViewRequest.setPostTitle("Title");
+        postViewRequest.setViewer(user);
+        viewService.viewPost(postViewRequest);
+        assertThat(viewRepository.count(), is(1L));
     }
 }
