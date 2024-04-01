e7cfc7e50b08833b8e702983bf3ba555ebdd8ec6
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-01 23:25:14 +0100

    blog

--- src/main/java/africa/semicolon/blog/datas/models/Comment.java
+++ src/main/java/africa/semicolon/blog/datas/models/Comment.java
@@ -2,7 +2,6 @@ package africa.semicolon.blog.datas.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
