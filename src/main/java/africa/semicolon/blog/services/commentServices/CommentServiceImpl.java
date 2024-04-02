1a06ef7759064aed482e6863ebd61a7c1f1624c0
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-02 16:59:23 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/commentServices/CommentServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/commentServices/CommentServiceImpl.java
@@ -2,6 +2,7 @@ package africa.semicolon.blog.services.commentServices;
 
 import africa.semicolon.blog.datas.models.Comment;
 import africa.semicolon.blog.datas.repositories.CommentRepository;
+import africa.semicolon.blog.dtos.request.postRequest.PostCommentEditRequest;
 import africa.semicolon.blog.dtos.request.postRequest.PostCommentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,10 @@ public class CommentServiceImpl implements CommentService {
         Comment comment = map(postCommentRequest);
         commentRepository.save(comment);
         return comment;
+    }
+
+    @Override
+    public void editComment(PostCommentEditRequest postCommentEditRequest) {
+
     }
 }
