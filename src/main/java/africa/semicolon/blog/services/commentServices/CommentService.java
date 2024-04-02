b84ee5f2e8f0e03eb27b49617f3af27d7c40906b
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-02 22:56:21 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/commentServices/CommentService.java
+++ src/main/java/africa/semicolon/blog/services/commentServices/CommentService.java
@@ -1,7 +1,6 @@
 package africa.semicolon.blog.services.commentServices;
 
 import africa.semicolon.blog.datas.models.Comment;
-import africa.semicolon.blog.dtos.request.postRequest.PostCommentEditRequest;
 import africa.semicolon.blog.dtos.request.postRequest.PostCommentRequest;
 
 public interface CommentService {
