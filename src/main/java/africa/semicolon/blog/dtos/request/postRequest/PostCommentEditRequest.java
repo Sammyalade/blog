4bc60966d224084e9175a35c1d2ac7370412bcaf
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-02 16:58:44 +0100

    blog

--- src/main/java/africa/semicolon/blog/dtos/request/postRequest/PostCommentEditRequest.java
+++ src/main/java/africa/semicolon/blog/dtos/request/postRequest/PostCommentEditRequest.java
@@ -1,4 +1,10 @@
 package africa.semicolon.blog.dtos.request.postRequest;
 
+import lombok.Data;
+
+@Data
 public class PostCommentEditRequest {
+
+    public String comment;
+    public String postTitle;
 }
