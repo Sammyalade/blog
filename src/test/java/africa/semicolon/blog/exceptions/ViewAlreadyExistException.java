ae80909df20309e792c345b48c142dda1d59acad
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-02 23:24:02 +0100

    blog

--- src/test/java/africa/semicolon/blog/exceptions/ViewAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package africa.semicolon.blog.exceptions;
-
-public class ViewAlreadyExistException extends BlogException{
-    public ViewAlreadyExistException(String message) {
-        super(message);
-    }
-}
