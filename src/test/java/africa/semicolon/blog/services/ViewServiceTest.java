9ee285734514605307304cd591bf7f0b5ce3d6e4
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-02 22:57:21 +0100

    blog

--- src/test/java/africa/semicolon/blog/services/ViewServiceTest.java
+++ src/test/java/africa/semicolon/blog/services/ViewServiceTest.java
@@ -1,8 +1,10 @@
 package africa.semicolon.blog.services;
 
+import africa.semicolon.blog.datas.models.Post;
 import africa.semicolon.blog.datas.models.User;
 import africa.semicolon.blog.datas.repositories.ViewRepository;
 import africa.semicolon.blog.dtos.request.postRequest.PostViewRequest;
+import africa.semicolon.blog.exceptions.ViewAlreadyExistException;
 import africa.semicolon.blog.services.viewServices.ViewService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class ViewServiceTest {
@@ -32,6 +35,22 @@ public class ViewServiceTest {
         postViewRequest.setPostTitle("Title");
         postViewRequest.setViewer(user);
         viewService.viewPost(postViewRequest);
+        assertThat(viewRepository.count(), is(1L));
+    }
+
+    @Test
+    public void testThatUserCannotViewAPostTwice(){
+        User user = new User();
+        PostViewRequest postViewRequest = new PostViewRequest();
+        Post post = new Post();
+        post.setTitle("Title");
+        postViewRequest.setPostTitle(post.getTitle());
+        postViewRequest.setViewer(user);
+        viewService.viewPost(postViewRequest);
+        PostViewRequest postViewRequest2 = new PostViewRequest();
+        postViewRequest2.setPostTitle(post.getTitle());
+        postViewRequest2.setViewer(user);
+        assertThrows(ViewAlreadyExistException.class, ()-> viewService.viewPost(postViewRequest2));
         assertThat(viewRepository.count(), is(1L));
     }
 }
