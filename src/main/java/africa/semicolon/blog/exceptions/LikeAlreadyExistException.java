3963b269798ea37499f2060c7888a1c5d661aaff
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-02 22:32:48 +0100

    blog

---
+++ src/main/java/africa/semicolon/blog/exceptions/LikeAlreadyExistException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.blog.exceptions;
+
+public class LikeAlreadyExistException extends BlogException {
+    public LikeAlreadyExistException(String message) {
+        super(message);
+    }
+}
