203aa8d2fec2aa6b061a1e6649d43a165ed8a517
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-02 22:33:04 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/likeServices/LikeServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/likeServices/LikeServiceImpl.java
@@ -3,6 +3,7 @@ package africa.semicolon.blog.services.likeServices;
 import africa.semicolon.blog.datas.models.Like;
 import africa.semicolon.blog.datas.repositories.LikeRepository;
 import africa.semicolon.blog.dtos.request.postRequest.PostLikeRequest;
+import africa.semicolon.blog.exceptions.LikeAlreadyExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,19 @@ public class LikeServiceImpl implements LikeService {
     @Override
     public Like makeLike(PostLikeRequest postLikeRequest) {
         Like like = map(postLikeRequest);
-        likeRepository.save(like);
-        return like;
+        if(!checkIfUserLikeExist(postLikeRequest)) {
+            likeRepository.save(like);
+            return like;
+        }
+        throw new LikeAlreadyExistException("Like already Exist");
+    }
+
+    private boolean checkIfUserLikeExist(PostLikeRequest postLikeRequest) {
+        for(Like like: likeRepository.findAll()){
+            if(like.getLikedBy().equals(postLikeRequest.getLikedBy()) &&
+                    like.getPostTitle().equals(postLikeRequest.getPostTitle()))
+                return true;
+        }
+        return false;
     }
 }
