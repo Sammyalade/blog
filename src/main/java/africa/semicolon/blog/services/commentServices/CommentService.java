a34bc3bc8baa91f6bbd8d96ecbd135129de7398c
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-14 23:41:23 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/commentServices/CommentService.java
+++ src/main/java/africa/semicolon/blog/services/commentServices/CommentService.java
@@ -1,8 +1,13 @@
 package africa.semicolon.blog.services.commentServices;
 
 import africa.semicolon.blog.datas.models.Comment;
+import africa.semicolon.blog.dtos.request.postRequest.CommentEditRequest;
 import africa.semicolon.blog.dtos.request.postRequest.PostCommentRequest;
 
 public interface CommentService {
     Comment makeComment(PostCommentRequest postCommentRequest);
+
+    void deleteComment(String id);
+
+    void editComment(CommentEditRequest commentEditRequest);
 }
--- src/main/java/africa/semicolon/blog/services/commentServices/CommentServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/commentServices/CommentServiceImpl.java
@@ -2,9 +2,13 @@ package africa.semicolon.blog.services.commentServices;
 
 import africa.semicolon.blog.datas.models.Comment;
 import africa.semicolon.blog.datas.repositories.CommentRepository;
+import africa.semicolon.blog.dtos.request.postRequest.CommentEditRequest;
 import africa.semicolon.blog.dtos.request.postRequest.PostCommentRequest;
+import africa.semicolon.blog.exceptions.CommentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 import static africa.semicolon.blog.utils.Mapper.map;
 
@@ -18,5 +22,22 @@ public class CommentServiceImpl implements CommentService {
         Comment comment = map(postCommentRequest);
         commentRepository.save(comment);
         return comment;
+    }
+
+    @Override
+    public void deleteComment(String id) {
+        commentRepository.deleteById(id);
+    }
+
+    @Override
+    public void editComment(CommentEditRequest commentEditRequest){
+        Optional<Comment> comment = commentRepository.findById(commentEditRequest.getCommentId());
+        if(comment.isPresent()){
+            comment.get().setCommentBody(commentEditRequest.getCommentBody());
+            commentRepository.save(comment.get());
+        }
+        else {
+            throw new CommentNotFoundException("Comment not found");
+        }
     }
 }
