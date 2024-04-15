d48bef13efe151ff7f84cb5a304b70d4291facf3
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-15 01:17:05 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/commentServices/CommentServiceImpl.java
+++ src/main/java/africa/semicolon/blog/services/commentServices/CommentServiceImpl.java
@@ -2,13 +2,11 @@ package africa.semicolon.blog.services.commentServices;
 
 import africa.semicolon.blog.datas.models.Comment;
 import africa.semicolon.blog.datas.repositories.CommentRepository;
-import africa.semicolon.blog.dtos.request.postRequest.CommentEditRequest;
-import africa.semicolon.blog.dtos.request.postRequest.PostCommentRequest;
+import africa.semicolon.blog.dtos.requests.CommentEditRequest;
+import africa.semicolon.blog.dtos.requests.PostCommentRequest;
 import africa.semicolon.blog.exceptions.CommentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 import static africa.semicolon.blog.utils.Mapper.map;
 
