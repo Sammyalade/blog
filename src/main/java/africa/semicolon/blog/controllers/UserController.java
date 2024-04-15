74e333edfc13ba8357d086bfcc47188cdda7db4c
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-15 01:09:07 +0100

    blog

---
+++ src/main/java/africa/semicolon/blog/controllers/UserController.java
@@ -0,0 +1,55 @@
+package africa.semicolon.blog.controllers;
+
+import africa.semicolon.blog.dtos.requests.UserRegistrationRequest;
+import africa.semicolon.blog.dtos.requests.UserUpdateRequest;
+import africa.semicolon.blog.dtos.requests.UserUpdateResponse;
+import africa.semicolon.blog.dtos.responses.UserApiResponse;
+import africa.semicolon.blog.dtos.responses.UserRegistrationResponse;
+import africa.semicolon.blog.exceptions.BlogException;
+import africa.semicolon.blog.services.userServices.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/blog")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/addUser")
+    public ResponseEntity<?> addUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+        try{
+            UserRegistrationResponse userRegistrationResponse = userService.registerUser(userRegistrationRequest);
+            return new ResponseEntity<>(new UserApiResponse(true, userRegistrationResponse), CREATED);
+        } catch (BlogException e){
+            return new ResponseEntity<>(new UserApiResponse(false , e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/updateUser")
+    public ResponseEntity<?> updateUser(@RequestBody UserUpdateRequest userUpdateRequest) {
+        try {
+            UserUpdateResponse userUpdateResponse = userService.updateUser(userUpdateRequest);
+            return new ResponseEntity<>(new UserApiResponse(true, userUpdateResponse), CREATED);
+        } catch (BlogException e){
+            return new ResponseEntity<>(new UserApiResponse(false , e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("/deleteUser/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable("userId") String userId) {
+        try{
+            userService.deleteUser(userId);
+            return new ResponseEntity<>(new UserApiResponse(true, "User Deleted Successfully"), HttpStatus.OK);
+        } catch (BlogException e){
+            return new ResponseEntity<>(new UserApiResponse(false , e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+}
