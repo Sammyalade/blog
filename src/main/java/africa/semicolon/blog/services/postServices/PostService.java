25dff8589aee66db0f3fb463135f96384e9fc149
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-15 02:06:21 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/postServices/PostService.java
+++ src/main/java/africa/semicolon/blog/services/postServices/PostService.java
@@ -1,14 +1,17 @@
 package africa.semicolon.blog.services.postServices;
 
+import africa.semicolon.blog.datas.models.Post;
 import africa.semicolon.blog.dtos.requests.*;
 
 public interface PostService {
 
-    void makePost(PostCreationRequest postCreationRequest);
+    Post makePost(PostCreationRequest postCreationRequest);
     void deletePost(String title);
     void updatePost(PostUpdateRequest postUpdateRequest);
     void makeComment(PostCommentRequest postLikeRequest);
     void makeLike(PostLikeRequest postLikeRequest);
     void viewPost(PostViewRequest postViewRequest);
     void removeLike(PostLikeRemoveRequest postLikeRemoveRequest);
+
+    Post findPostById(String postId);
 }
