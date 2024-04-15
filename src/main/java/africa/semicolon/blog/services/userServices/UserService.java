d272ecb6f98dc7b2e8270856850b4d2f19bc681a
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-15 01:21:12 +0100

    blog

--- src/main/java/africa/semicolon/blog/services/userServices/UserService.java
+++ src/main/java/africa/semicolon/blog/services/userServices/UserService.java
@@ -1,4 +1,22 @@
 package africa.semicolon.blog.services.userServices;
 
+import africa.semicolon.blog.datas.models.User;
+import africa.semicolon.blog.dtos.requests.UserLoginRequest;
+import africa.semicolon.blog.dtos.requests.UserRegistrationRequest;
+import africa.semicolon.blog.dtos.requests.UserUpdateRequest;
+import africa.semicolon.blog.dtos.requests.UserUpdateResponse;
+import africa.semicolon.blog.dtos.responses.UserRegistrationResponse;
+
 public interface UserService {
+    UserRegistrationResponse registerUser(UserRegistrationRequest userRegistrationRequest);
+
+    User searchUserById(String userId);
+
+    void loginUser(UserLoginRequest userLoginRequest);
+
+    void logoutUser(String id);
+
+    UserUpdateResponse updateUser(UserUpdateRequest userUpdateRequest);
+
+    void deleteUser(String id);
 }
