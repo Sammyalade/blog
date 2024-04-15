3af6c59a2c1e58416af5cc6f61c3b4dec894e756
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-15 02:04:44 +0100

    blog

---
+++ src/main/java/africa/semicolon/blog/dtos/responses/PostCreationResponse.java
@@ -0,0 +1,11 @@
+package africa.semicolon.blog.dtos.responses;
+
+import lombok.Data;
+
+@Data
+public class PostCreationResponse {
+
+    private String PostId;
+    private String PostTitle;
+    private String userid;
+}
